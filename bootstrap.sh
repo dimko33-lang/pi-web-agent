b5277ba4f0530b00a400c861830127582e96cd9d
dimko33-lang <dimko33@gmail.com>
2026-04-02 19:24:59 +0300

    add bootstrap script
    Add a bootstrap script for setting up the environment

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,20 @@
 
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+REPO_URL="${REPO_URL:-https://github.com/dimko33-lang/pi-web-agent.git}"
+WORKDIR="/root/pi-web-agent-src"
+
+if [ "$(id -u)" -ne 0 ]; then
+  echo "Please run as root (or with sudo)."
+  exit 1
+fi
+
+apt update
+apt install -y git
+
+rm -rf "$WORKDIR"
+git clone "$REPO_URL" "$WORKDIR"
+
+cd "$WORKDIR"
+bash install.sh
